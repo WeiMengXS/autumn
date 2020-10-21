f50fe7402f07a2c97157b7e2894cba313d22c252
Wmxs <1254688559@qq.com>
2020-10-21 21:52:56 +0800

    snv

---
+++ Project8/Project8/test.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#define M 5
+#define N M+M
+using namespace std;
+class a
+{
+	a(){}
+	virtual ~a()
+	{}
+	char d;
+};
+int main()
+{
+
+		char cArray[16] = "hello world!";
+	
+		printf("%dr\n",sizeof(a));
+			system("pause");
+
+
+	return 0;
+}
